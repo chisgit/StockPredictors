5236f0c4c5d176181ba03863e01f9292cfcfba70
chisgit <chiragpatel@hotmail.com>
2025-02-10 17:48:11 -0800

    Removed Adj close as yfinance 0.2.52 automatically adjusts the prices

--- data_handler.py
+++ data_handler.py
@@ -11,14 +11,14 @@ def fetch_data(ticker, end_date, start_date='2008-01-01'):
     # Get current date in NYSE timezone
     # end_date = get_nyse_date() removed this because we add 1 day already
     
-    # Download data with adjusted prices
+    # Download data with adjusted prices #not 0.2.52 automatically adjusts the prices for stock splits
     df = yf.download(ticker, start=start_date, end=end_date)
     
-    # Use adjusted prices to handle stock splits correctly
-    df['Open'] = df['Adj Close'] * df['Open'] / df['Close']
-    df['High'] = df['Adj Close'] * df['High'] / df['Close']
-    df['Low'] = df['Adj Close'] * df['Low'] / df['Close']
-    df['Close'] = df['Adj Close']
+    # Only need this code for older yfinance versions
+    # df['Open'] = df['Adj Close'] * df['Open'] / df['Close']
+    # df['High'] = df['Adj Close'] * df['High'] / df['Close']
+    # df['Low'] = df['Adj Close'] * df['Low'] / df['Close']
+    # df['Close'] = df['Adj Close']
     
     # Create MultiIndex columns
     cols = ['Open', 'High', 'Low', 'Close', 'Volume']  # Each price type is its own level
